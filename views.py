ecd144cf8effe6bddd4d9b0456cbeddefa4d788e
paulwritescode <paulkingdev7@gmail.com>
2024-02-06 22:01:05 +0300

    routes added

--- views.py
+++ views.py
@@ -6,3 +6,14 @@ app = Flask(__name__)
 @app.route("/")
 def login():
     return render_template('login.html')
+
+
+# admin route
+@app.route("/admin/")
+def adminRoute():
+    return render_template('admin/index.html')
+
+# user route
+@app.route("/user/")
+def userRoute():
+    return render_template('user/index.html')
