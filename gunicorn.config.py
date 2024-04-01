3cf9cff2e7668b34749e1d225f11d6618f042c3d
jimyjames <sjmuriithi@gmail.com>
2024-04-01 20:23:33 +0300

    gunicorn services added

---
+++ gunicorn.config.py
@@ -0,0 +1,6 @@
+import os
+from dotenv import load_dotenv
+for env_file in ('.env', '.flaskenv'):
+    env = os.path.join(os.getcwd(), env_file)
+    if os.path.exists(env):
+        load_dotenv(env)
