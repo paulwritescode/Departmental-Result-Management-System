67c89b0ca5524ad128851a29680722cc20f4e4ac
emmanuel-mwendwa <mwendwae054@gmail.com>
2024-04-02 07:04:18 +0000

    Database connection update

--- config.py
+++ config.py
@@ -7,21 +7,16 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     FLASKY_ADMIN = os.environ.get("FLASKY_ADMIN")
 
+
     @staticmethod
     def init_app(app):
         pass
 
 
 class DevelopmentConfig(Config):
-    DATABASE_HOSTNAME=os.environ.get("DATABASE_HOSTNAME")
-    DATABASE_PORT=os.environ.get("DATABASE_PORT")
-    DATABASE_PASSWORD=os.environ.get("DATABASE_PASSWORD")
-    DATABASE_NAME=os.environ.get("DATABASE_NAME")
-    DATABASE_USERNAME=os.environ.get("DATABASE_USERNAME")
     DEBUG=True
 
-    SQLALCHEMY_DATABASE_URI = f'postgresql://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@{DATABASE_HOSTNAME}:{DATABASE_PORT}/{DATABASE_NAME}'
-
+    SQLALCHEMY_DATABASE_URI = f'postgresql://postgres:root@localhost:5432/elimusawa'
 
 
 config = {"development": DevelopmentConfig, "default": DevelopmentConfig}
--- rms.py
+++ rms.py
@@ -1,4 +1,5 @@
 import os
+from dotenv import load_dotenv
 
 from flask_migrate import Migrate
 
